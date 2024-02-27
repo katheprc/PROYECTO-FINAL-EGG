4b74ac9fda019f6fd5dcae2f3937315b52dc8a48
katheprc <yaelvaldez@gmail.com>
2024-02-27 17:11:22 -0300

    Creación de proyecto

---
+++ src/main/java/com/grupo1/PROYECTOFINALEGG/ProyectoFinalEggApplication.java
@@ -0,0 +1,13 @@
+package com.grupo1.PROYECTOFINALEGG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalEggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalEggApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo1/PROYECTOFINALEGG/ProyectoFinalEggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo1.PROYECTOFINALEGG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalEggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
