c80ebbb8d79416322c113fccadb6c4a43ab9d6a5
Emi1000se <129129683+Mariaredes@users.noreply.github.com>
2024-03-07 19:24:02 -0300

    repository

---
+++ src/main/java/com/grupo1/PROYECTOFINALEGG/Repositories/BookingRepository.java
@@ -0,0 +1,14 @@
+package com.grupo1.PROYECTOFINALEGG.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.grupo1.PROYECTOFINALEGG.Entity.Booking;
+
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Integer>{
+
+  
+	
+}
