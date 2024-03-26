8bef0f96eb3edd85d768e6a8d51f0303f9739b87
katheprc <yaelvaldez@gmail.com>
2024-03-25 22:31:27 -0300

    -

--- src/main/java/com/grupo1/PROYECTOFINALEGG/controller/rentalController.java
+++ src/main/java/com/grupo1/PROYECTOFINALEGG/controller/rentalController.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
--- src/main/java/com/grupo1/PROYECTOFINALEGG/service/RentalService.java
+++ src/main/java/com/grupo1/PROYECTOFINALEGG/service/RentalService.java
@@ -2,7 +2,6 @@ package com.grupo1.PROYECTOFINALEGG.service;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
